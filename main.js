aefe86c25c105f92241495e47fdf33fae1fd0826
oguzhan cicek <oguzhancicek01@hotmail.com>
2024-11-16 15:44:36 +0300

    update

--- main.js
+++ main.js
@@ -1,70 +1,64 @@
-let point1 = 0;
+let teamAScore = 0;
+let teamBScore = 0;
 
-const point3 = document.getElementById("point1");
+const teamAScoreElement = document.getElementById("teamA-score");
+const teamBScoreElement = document.getElementById("teamB-score");
 
-function clickcounter(){
-    point1 = point1 + 1;
-    point3.innerHTML = point1;
+function increaseScoreA() {
+  teamAScore = teamAScore + 1;
+  teamAScoreElement.innerHTML = teamAScore;
 }
 
-function clickcounter3(){
-    if (point1 > 0){
-        point1 = point1 - 1;
+function decreaseScoreA() {
+  if (teamAScore > 0) {
+    teamAScore = teamAScore - 1;
+  }
+  teamAScoreElement.innerHTML = teamAScore;
+}
+
+function increaseScoreB() {
+  teamBScore = teamBScore + 1;
+  teamBScoreElement.innerHTML = teamBScore;
+}
+
+function decreaseScoreB() {
+  if (teamBScore > 0) {
+    teamBScore = teamBScore - 1;
+  }
+  teamBScoreElement.innerHTML = teamBScore;
+}
+
+function changeScoreA() {
+  const newScore = prompt("Sadece sayı giriniz");
+  if (newScore != Number(newScore)) {
+    alert("Sistemin bozuldu !!!!");
+  } else {
+    teamAScore = Number(newScore);
+    teamAScoreElement.innerHTML = teamAScore;
+  }
+}
+
+function changeScoreB() {
+    const newScore = prompt("Sadece sayı giriniz");
+    if (newScore != Number(newScore)) {
+      alert("Sistemin bozuldu !!!!");
     }
-    point3.innerHTML = point1;
-}
-
-let point2 = 0;
-
-const point4 = document.getElementById("point2");
-
-function clickcounter2(){
-    point2 = point2 + 1;
-    point4.innerHTML = point2;
-}
-
-function clickcounter4(){
-    if (point2 > 0){
-        point2 = point2 - 1;
+    else {
+        teamBScore = Number(newScore);
+        teamBScoreElement.innerHTML = teamBScore;
     }
-    point4.innerHTML = point2;
+  }
+
+function changeNameA() {
+  const newName = prompt("Takımın ismi ne olsun");
+  if (newName) {
+    document.getElementById("teamA-name").innerHTML = newName;
+  }
 }
 
-let changeteam2 = document.getElementById("changeteam");
-
-function myFunction(){
-    person = prompt();
-    document.getElementById("changeteam").innerHTML = person;
-}
-
-let changeteam3 = document.getElementById("changeteam2");
-
-function myfunction2(){
-    person2 = prompt();
-    document.getElementById("changeteam2").innerHTML = person2;
-}
-
-
-
-
-function myFunction3(){
-    pointcheck = prompt()
-    if (pointcheck != Number(pointcheck)){
-        pointcheck = alert("Sistemin içine ettin !!!!")
-
-        pointcheck = "??!!"
-    }
-    
-    document.getElementById("point1").innerHTML = pointcheck;
-}
-
-function myfunction4(){
-    pointcheck2 = prompt();
-    if (pointcheck2 != Number(pointcheck2)){
-        pointcheck2 = alert("Sistemin içine ettin !!!!")
-
-        pointcheck2 = "??!!"
-    }
-
-    document.getElementById("point2").innerHTML = pointcheck2;
+function changeNameB() {
+  const newName = prompt("Takımın ismi ne olsun");
+  if (newName) {
+    document.getElementById("teamB-name").innerHTML = newName;
+  }
 }
